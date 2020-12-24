1512d842fe97d38ddb13d8ce99546ae5e567675d
huangds <hds@rock-chips.com>
2020-12-24 15:37:10 +0800

    add dralines, mosic,quadrilateral function

--- glprocess.h
+++ glprocess.h
@@ -12,14 +12,58 @@
 #define EGL_YUV_NARROW_RANGE_EXT          0x3283
 #define OP_RENDER_TEXTURE 0
 #define OP_COLOR_FILL    1
+#define OP_DRAW_LINES    2 
 
 
 #define MAXLayer 160 
+
+#define MAXLines 64
+
+typedef struct point {
+    int x;
+    int y;  
+} point_t;
+
+typedef struct line{
+    point_t startPt;
+    point_t endPt;
+    int lineW;
+    float color[3];
+} line_t;
+
+typedef struct lines{
+    line_t linesArray[MAXLines];
+    int numlines;
+} lines_t;
+
+typedef struct quadrl_info {
+    float color[3];
+    int coord[8];  
+    // [0]=x0 [1]=y0, [2]=x1 [3]=y1,  [4]=x2 [5]=y2,  [6]=x3 [7]=y3
+    //        (x0,y0)------- (x3,y3)
+    //               |     |
+    //               |     |
+    //               |     |
+    //        (x1,y1)------- (x2,y2)
+    
+    int lineW;
+    int quadBox;   // If you want to render the border this flag is set to 1
+} quadrl_t;
+
+typedef struct _mosaic_info{
+    int left;
+    int top;
+    int right;
+    int bottom;
+    int bsize;
+    int mode;
+} mosaic_info_t;
+
+
 typedef struct layer_info {
     int fd;
     int afbc_flag;
     int format;
-    float cfillRgb[3];
     int color_space ;
     int sample_range;
     int width;
@@ -29,17 +73,22 @@ typedef struct layer_info {
     int right;
     int bottom;
     int focuswin;
+    int rotation;
+    quadrl_t quadril_info;
+    mosaic_info_t mosic_info;
 } layer_info_t;
 
 typedef struct layer_list {
     layer_info_t srcLayerInfo[MAXLayer];
     layer_info_t dstLayerInfo[MAXLayer];
+    lines_t lineSet;
     int numLayer;
     int op;
     float wfRgb[3];
     float fcfRgb[3];
     int px;
     int pxfc;
+    int imgReserve;
 } layer_list_t;
 
 #ifdef __cplusplus
